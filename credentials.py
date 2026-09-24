8344a7822d52a1b932843d7320937dc5fa99f9d5
Moazzam <moazzamaleem786@gmail.com>
2026-09-24 21:22:59 +0500

    security: remove exposed credentials.json/credentials.py, replace with placeholders

---
+++ credentials.py
@@ -0,0 +1,7 @@
+credentials = {
+    "usernames": {
+        "admin": {"name": "Admin", "password": "CHANGE_ME_ADMIN", "role": "admin"},
+        "user1": {"name": "User1", "password": "CHANGE_ME_USER1", "role": "user"},
+        "user2": {"name": "User2", "password": "CHANGE_ME_USER2", "role": "user"},
+    }
+}
